4dd99eb7d4b3a2a4ff1973341a24252d2ba141e1
Mayara Ascari <Mayara.Ascari@ibm.com>
2022-06-24 10:50:48 -0300

    first commit

---
+++ Carros.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Carros : IComparable
+    {
+        public string PlacaDoModelo { get; set; }
+        private double KmInicial { get; set; }
+        private double KmFinal { get; set; }
+
+        public Carros(string placaDoModelo)
+        {
+            PlacaDoModelo = placaDoModelo;
+        }
+
+        public Carros(string placaDoModelo, double kmInicial, double kmFinal) : this(placaDoModelo)
+        {
+            KmInicial = kmInicial;
+            KmFinal = kmFinal;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Carros))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+
+            KmFinal -= KmInicial;
+            Carros other = obj as Carros;
+            return KmFinal.CompareTo(other.KmFinal);
+        }
+
+        public override string ToString()
+        {
+            return PlacaDoModelo
+                + ", "
+                + KmFinal.ToString();
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verificação de Quilometragens rodadas dos veiculos\n");
+
+            List<Carros> lista = new List<Carros>();
+            Console.Write("Quantidade de veículos a serem verificados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double kmInicial = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                double kmFinal = double.Parse(vect[2], CultureInfo.InvariantCulture);
+                lista.Add(new Carros(vect[0], kmInicial, kmFinal));
+            }
+
+            CalculationService calculation = new CalculationService();
+            Carros quilometragem = calculation.Max(lista);
+            Console.WriteLine("\nVeículo com mais quilômetros rodados: ");
+            Console.WriteLine(quilometragem);
+        
+        }
+    }
+}
